30993a12e78f6b719f8100eaa88a8fd4d26839bb
willianavila <williandeavila@gmail.com>
2017-08-06 20:05:21 -0300

    melhorias no layout

--- index.php
+++ index.php
@@ -83,7 +83,7 @@
 			<div class="row">
 				<div class="col-md-12">
 					<div class="jumbotron">
-						<img style="float: left;" src="img/logo.png" alt="Tear Inovações" height="150" width="150"><h2 style="text-align: left;"><strong>Votação de logo Projeto AURORA</strong></h2>
+						<img style="float: left;" src="img/site/logo.png" alt="Tear Inovações" height="150" width="150"><h2 style="text-align: left;"><strong>Votação de logo Projeto AURORA</strong></h2>
 						<div style="margin-left: 150px;">
 							<p style="text-align: justify; width: 90%;">
 								A escolha será feita observados os seguintes itens: representação da identidade do Projeto, identificação da missão e valores do Projeto, participação colaborativa dos envolvidos, criatividade, inovação. Qual desenho melhor representa a identidade do Projeto Aurora?
@@ -93,22 +93,22 @@
 					<div class="row">
 						<div class="col-md-1"></div>
 						<div class="col-md-4">
-							<a href='index.php?<?php echo "won=$random1&lost=$random2"; ?>'><img src="img/image(<?php echo $random1; ?>).jpg" class="img-rounded" style="float: right; height:300px; max-width: 400px;"/></a>
+							<a href='index.php?<?php echo "won=$random1&lost=$random2"; ?>'><img src="img/image(<?php echo $random1; ?>).jpg" class="img-rounded" style="height:300px; max-width: 400px;"/></a>
 						</div>
 						<div class="col-md-2">
-							<p style='text-align: center; vertical-align: middle; margin-top: 150px;'>
+							<p style='text-align: center; vertical-align: middle; margin-top: 130px; margin-bottom: 130px;'>
 								<strong>OU</strong>
 							</p>
 						</div>
 						<div class="col-md-4">
-							<a href='index.php?<?php echo "won=$random1&lost=$random2"; ?>'><img  src="img/image(<?php echo $random2; ?>).jpg" class="img-rounded" style="float: left; height:300px; max-width: 400px;"/></a>
+							<a href='index.php?<?php echo "won=$random1&lost=$random2"; ?>'><img src="img/image(<?php echo $random2; ?>).jpg" class="img-rounded" style="height:300px; max-width: 400px;"/></a>
 						</div>
 						<div class="col-md-1"></div>
 					</div>
+					<img style="padding-top: 30px;" src="img/site/sombra.png"/>
+					<div style="padding-top: 15px;" class="rating">Rating:<?php echo $ratingA; ?>/ Rating: <?php echo $ratingB; ?></div>
+					<div class="footer"><h4><strong>Escolas Inovadoras de Viamão</strong></h4></div>
 				</div>
-				</br></br></br></br>
-				<div class="rating">Rating:<?php echo $ratingA; ?>/ Rating: <?php echo $ratingB; ?></div>
-				<div class="footer"><h4><strong>Escolas Inovadoras de Viamão</strong></h4></div>
 			</div>
 		</div>
 	</body>
